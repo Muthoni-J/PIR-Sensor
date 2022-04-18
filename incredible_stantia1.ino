111bc16d1d9cb32d27dca2e461efcc78ef3bae3d
Muthoni-J <99389198+Muthoni-J@users.noreply.github.com>
2022-04-18 10:06:58 +0300

    Add files via upload

---
+++ incredible_stantia1.ino
@@ -0,0 +1,25 @@
+// C++ code
+//
+int pirsensor = 0;
+
+void setup()
+{
+  pinMode(5,INPUT);
+  pinMode(13,OUTPUT);
+}
+
+void loop()
+{
+
+ pirsensor = digitalRead(5);
+ if(pirsensor == HIGH)
+ {      
+   digitalWrite(13,HIGH);
+ }
+ else
+ { 
+   digitalWrite(13,LOW);
+ }
+ 
+ delay(10);  
+}
